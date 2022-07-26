c9957397242c211f6b88c980d0eefd8db6f6b486
Cassiano Flores <cassiano.flores2424@gmail.com>
2022-07-26 03:49:11 -0300

    board exception and defensive programming

--- boardgame/BoardException.java
+++ boardgame/BoardException.java
@@ -1,6 +1,6 @@
 package boardgame;
 
-//personalized Exception class to write any error message 
+//personalized BoardException class to write any error message 
 public class BoardException extends RuntimeException {
     
     public BoardException(String msg){
